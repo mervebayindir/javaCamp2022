74961bbced2f53075aa3f8cd092acb80b8c03d78
Mert Topal <87638248+merrttopal@users.noreply.github.com>
2022-09-26 02:37:20 +0300

    reCapDemoClasses

---
+++ reCapDemoClasses/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        FourOperations fourOperations = new FourOperations();
+        int total = fourOperations.sum(3,4);
+        //fourOperations.extraction(4,2);
+        //fourOperations.multiplication(4,2);
+        //fourOperations.dividing(4,2);
+
+        System.out.println(total);
+
+    }
+}
