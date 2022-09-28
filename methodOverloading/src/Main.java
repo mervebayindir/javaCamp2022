07f9aa94a98337402562de82dafe81beac5957d6
Mert Topal <87638248+merrttopal@users.noreply.github.com>
2022-09-28 20:27:02 +0300

    methodOverloading

---
+++ methodOverloading/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        FourOperation fourOperation = new FourOperation();
+
+        System.out.println(fourOperation.sum(2,4));
+        System.out.println(fourOperation.sum(2,4,6));
+    }
+}
