8081555d3419d6b46924378bda7ff4d6855f718c
Merve Bayindir <76398913+mervebayindir@users.noreply.github.com>
2023-04-30 15:07:22 +0300

    Delete youtubeEgitim directory

--- youtubeEgitim/src/BaseCreditManager.java
+++
@@ -1,6 +0,0 @@
-public abstract class BaseCreditManager implements ICreditManager {
-    public abstract void Calculate();
-    public static void Save(){
-        System.out.println("Saved");
-    }
-}
--- youtubeEgitim/src/MilitaryCreditManager.java
+++
@@ -1,9 +0,0 @@
-public class MilitaryCreditManager implements ICreditManager{
-
-    public void Calculate(){
-        System.out.println("Military Credit Calculated");
-    }
-    public void Save(){
-
-    }
-}
